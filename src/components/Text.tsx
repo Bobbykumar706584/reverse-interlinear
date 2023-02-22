c96c752e6a8def64806e82866077dd84d83dc322
revant.cherian <revant.cherian@bridgeconn.com>
2023-02-22 22:12:43 +0530

    add Bible api and bible text

---
+++ src/components/Text.tsx
@@ -0,0 +1,33 @@
+import { useQuery } from "react-query";
+
+const fetcher = async (url: string) => {
+  const res = await fetch(url);
+  const data = await res.json();
+
+  if (res.status !== 200) {
+    throw new Error(data.message);
+  }
+  return data;
+};
+
+const Text = ({ book, chapter }: { book: string; chapter: number }) => {
+  const { isLoading, data } = useQuery("repoData", () =>
+    fetch("/api/bible/mat/2").then((res) => res.json())
+  );
+  return (
+    <div>
+      {data.contents?.map(
+        (item: { verseNumber: number; verseText: string }) => {
+          return (
+            <>
+              <span>{item.verseNumber}</span>
+              <span>{item.verseText}</span>
+            </>
+          );
+        }
+      )}
+    </div>
+  );
+};
+
+export default Text;
