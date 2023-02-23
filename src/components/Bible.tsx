2c29e34f97c4c39bb962a6aa4cb3485336c24295
revant.cherian <revant.cherian@bridgeconn.com>
2023-02-23 16:44:16 +0530

    add chapter drop down

--- src/components/Bible.tsx
+++ src/components/Bible.tsx
@@ -1,18 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Books from "./Books";
+import Chapters from "./Chapters";
 import Text from "./Text";
+
 type OptionType = {
   label: string;
   value: string;
 };
 const Bible = () => {
-  const [book, setBook] = useState<OptionType | null>();
+  const [book, setBook] = useState<OptionType | undefined>({
+    label: "Genesis",
+    value: "gen",
+  });
+  const [chapter, setChapter] = useState<OptionType | undefined>();
+  useEffect(() => {
+    console.log("test");
+    setChapter({ value: "1", label: "1" });
+  }, [book]);
   return (
     <>
       <div>
-        <Books setBook={setBook} />
+        <Books book={book} setBook={setBook} />
       </div>
-      <div>Chapter</div>
+      <div>
+        <Chapters book={book} chapter={chapter} setChapter={setChapter} />
+      </div>
       <div>Verse</div>
       <div>
         <Text book="mat" chapter={1} />
