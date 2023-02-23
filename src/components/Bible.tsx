36a917acb9593cd72ec1ac68fc66ceeb3e48893a
revant.cherian <revant.cherian@bridgeconn.com>
2023-02-23 16:35:20 +0530

    Revert duplicate code as chapters has been done

--- src/components/Bible.tsx
+++ src/components/Bible.tsx
@@ -1,20 +1,18 @@
 import { useState } from "react";
 import Books from "./Books";
 import Text from "./Text";
-import  Chapters  from "./Chapters";
 type OptionType = {
   label: string;
   value: string;
 };
 const Bible = () => {
   const [book, setBook] = useState<OptionType | null>();
-  const [chapter, setChapter] = useState<number>(1)
   return (
     <>
       <div>
         <Books setBook={setBook} />
       </div>
-      <div><Chapters /></div>
+      <div>Chapter</div>
       <div>Verse</div>
       <div>
         <Text book="mat" chapter={1} />
